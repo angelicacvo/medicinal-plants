0bff5382813ee036ffb7912cc4e1afe279651089
Angelica Cuervo <108093638+angelicacvo@users.noreply.github.com>
2025-10-31 06:37:26 -0500

    feat: users CRUD created

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../users/entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.usersRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    if (user) {
+      throw new ConflictException('El usuario ya existe');
+    }
+
+    const newUser = this.usersRepository.create(createUserDto);
+    return await this.usersRepository.save(newUser);
+  }
+  
+  async findAll(): Promise<User[]> {
+    return await this.usersRepository.find();
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findOne(id: number): Promise<User> {
+    const user = await this.usersRepository.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException(`Usuario con ID ${id} no encontrado`);
+    }
+    return user;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.usersRepository.findOneBy({ id });
+
+    if (!user) {
+      throw new NotFoundException(`Usuario con ID ${id} no encontrado`);
+    }
+    const existingUser = await this.usersRepository.findOne({
+      where: { email: updateUserDto.email },
+    });
+    if (existingUser && existingUser.id !== id) {
+      throw new ConflictException('El email ya está en uso por otro usuario');
+    }
+    Object.assign(user, updateUserDto);
+    return this.usersRepository.save(user);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number): Promise<void> {
+    const user = await this.usersRepository.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException(`Usuario con ID ${id} no encontrado`);
+    }
+    await this.usersRepository.delete(id);
   }
 }
+
+
+
